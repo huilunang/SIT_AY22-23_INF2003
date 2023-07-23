c0fa2a2a76e8af8ec3f8e1d6401fe3d704a4aeee
huilunang <huilun2000@gmail.com>
2023-07-24 04:06:20 +0800

    Organised queries and linted syntax

--- src/app.py
+++ src/app.py
@@ -1,76 +1,86 @@
-from database.mongodb_conn import MongoDBConnManager
-
-from flask import Flask, render_template, request, session, redirect, url_for, jsonify
-
-import mariadb_queries as db
-
 import re
 import hashlib
 
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
+
+import utils.helper_functions as helper
+import utils.mariadb_queries as maria_q
+import utils.mongodb_queries as mongo_q
 
 app = Flask(__name__)
-
-mongo_db = MongoDBConnManager()
-
 # session data signed by server cryptographically
 app.secret_key = "secret key"
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login",methods=['GET','POST'])
-def login():
-    msg=''
-    if request.method == 'POST':
-        username=request.form['username']
-        password=request.form['password']
-        hashed_password=hashlib.sha256(password.encode()).hexdigest()
-        record=db.authenticate(username, hashed_password)
-        if record:
-            session['loggedin']=True
-            session['id']=record[0]
-            session['username']=record[4]
-            return redirect(url_for('home')) 
-        else:
-            msg='Incorrect credentials entered. Please check your username/password.'
-    return render_template('login.html',msg=msg)
 
-@app.route("/register", methods=['GET','POST'])
-def register():
-    msg = ''
-    if request.method == 'POST' and 'name' in request.form and 'email' in request.form and 'area' in request.form and 'username' in request.form and 'password' in request.form:
-        name = request.form['name']
-        email = request.form['email']
-        area = request.form['area']
-        username = request.form['username']
-        password = request.form['password']
-        record = db.regCheck(username)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    msg = ""
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        record = maria_q.authenticate(username, hashed_password)
+
         if record:
-            msg = 'Username/Account already exists !'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address !'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
+            session["loggedin"] = True
+            session["id"] = record[0]
+            session["username"] = record[4]
+            return redirect(url_for("home"))
+
+        msg = "Incorrect credentials entered. Please check your username/password."
+    return render_template("login.html", msg=msg)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    msg = ""
+    if (
+        request.method == "POST"
+        and "name" in request.form
+        and "email" in request.form
+        and "area" in request.form
+        and "username" in request.form
+        and "password" in request.form
+    ):
+        name = request.form["name"]
+        email = request.form["email"]
+        area = request.form["area"]
+        username = request.form["username"]
+        password = request.form["password"]
+        record = maria_q.regCheck(username)
+
+        if record:
+            msg = "Username/Account already exists !"
+        elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            msg = "Invalid email address !"
+        elif not re.match(r"[A-Za-z0-9]+", username):
+            msg = "Username must contain only characters and numbers !"
         elif not name or not email or not area or not username or not password:
-            msg = 'Please fill in all the fields!'
+            msg = "Please fill in all the fields!"
         else:
-            hashed_password=hashlib.sha256(password.encode()).hexdigest()
-            db.register(name, email, area, username, hashed_password)
-            msg = 'You have successfully registered!'
-            return redirect(url_for('login'))
-    elif request.method == 'POST':
-        msg = 'Please fill in all the fields!'
-    return render_template('register.html', msg = msg)
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
+            maria_q.register(name, email, area, username, hashed_password)
+            msg = "You have successfully registered!"
+            return redirect(url_for("login"))
+    elif request.method == "POST":
+        msg = "Please fill in all the fields!"
+    return render_template("register.html", msg=msg)
+
 
 @app.route("/home")
 def home():
     # user current points
-    record = db.getUserPoints()
+    record = maria_q.getUserPoints()
     points = record[0]
 
     # recycles by month line chart
-    record = db.getRecyclesByMonth()
+    record = maria_q.getRecyclesByMonth()
     recycles_by_month = []
     month_label = []
     for date, num in record:
@@ -78,130 +88,131 @@ def home():
         recycles_by_month.append(num)
 
     # materials count bar chart
-    record = db.getMaterialCount()
+    record = maria_q.getMaterialCount()
     material_count = []
     material = []
     for mat, num in record:
         material.append(mat)
         material_count.append(num)
 
-    return render_template('home.html', username=session['username'], points=points, recycles_by_month=recycles_by_month, month_label=month_label, material=material, material_count=material_count)
+    return render_template(
+        "home.html",
+        username=session["username"],
+        points=points,
+        recycles_by_month=recycles_by_month,
+        month_label=month_label,
+        material=material,
+        material_count=material_count,
+    )
 
-@app.route("/profile",methods=['GET','POST'])
+
+@app.route("/profile", methods=["GET", "POST"])
 def profile():
-    msg=''
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        area = request.form['area']
-        username = request.form['username']
-        password = request.form['password']
-        if name == "" or email=="" or area == "" or username == "":
-            msg = 'Please ensure all fields are filled in.'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address !'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
+    msg = ""
+
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        area = request.form["area"]
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if name == "" or email == "" or area == "" or username == "":
+            msg = "Please ensure all fields are filled in."
+        elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            msg = "Invalid email address !"
+        elif not re.match(r"[A-Za-z0-9]+", username):
+            msg = "Username must contain only characters and numbers !"
         else:
             if password == "":
-                db.updateProfile(name, email, area, username)
-                msg = 'Profile Updated Successfully.'
-                session['username']=username
+                maria_q.updateProfile(name, email, area, username)
+                msg = "Profile Updated Successfully."
+                session["username"] = username
             else:
-                hashed_password=hashlib.sha256(password.encode()).hexdigest()
-                db.updateProfileWPwd(name, email, area, username, hashed_password)
-                msg = 'Profile Updated Successfully.'
-                session['username']=username
-    details=db.userProfile()
-    return render_template('profile.html',msg=msg, details=details)
+                hashed_password = hashlib.sha256(password.encode()).hexdigest()
+                maria_q.updateProfileWPwd(name, email, area, username, hashed_password)
+                msg = "Profile Updated Successfully."
+                session["username"] = username
 
-@app.route('/search', methods=['GET', 'POST'])
+    details = maria_q.userProfile()
+
+    return render_template("profile.html", msg=msg, details=details)
+
+
+@app.route("/search", methods=["GET", "POST"])
 def search():
-    query = request.args.get('q')  # Access the 'q' query parameter
-    mquery = request.args.get('m')  # Access the 'q' query parameter
-    # if query:
-    #     conn = maria_db.get_conn()
-    #     cur = conn.cursor()
-    #     cur.execute("SELECT * FROM info1 INNER JOIN info2 ON info1.id = info2.id WHERE info1.item = %s", (query,))
-    #     record = cur.fetchone()
+    query = request.args.get("q")  # Access the 'q' query parameter
+    mquery = request.args.get("m")  # Access the 'm' query parameter
 
-    #     return render_template('qsearch.html', query=query, record=record)
-    # elif mquery:
-    #     mrecord = []
-    #     conn = maria_db.get_conn()
-    #     cur = conn.cursor()
-    #     cur.execute("SELECT * FROM info1 INNER JOIN info2 ON info1.id = info2.id WHERE info1.type = %s", (mquery,))
-    #     row_headers = [x[0] for x in cur.description]
-    #     rv = cur.fetchall()
+    if query or mquery:
+        data = {"query": query}
 
-    #     for result in rv:
-    #         mrecord.append(dict(zip(row_headers, result)))
+        if query:
+            data["record"] = maria_q.get_search("q", query)
+        else:
+            data["mrecord"] = maria_q.get_search("m", mquery)
 
-    #     return render_template('qsearch.html', query=query, mrecord=mrecord)
-    # else:
-    #     if request.method == 'POST':
-    #         search_query = request.form.get('search_query')
-    #         suggested_words = retrieve_suggested_words_from_database(search_query)
-    #         return render_template('search.html', suggested_words=suggested_words)
-    #     return render_template('search.html')
-    return render_template('search.html') # to remove
+        return render_template("qsearch.html", data=data)
+    elif request.method == "POST":
+        search_query = request.form.get("search_query")
+        suggested_words = helper.get_suggestions(search_query)
 
-def retrieve_suggested_words_from_database(search_query):
-    if len(search_query) >= 2:
-        conn = maria_db.get_conn()
-        cur = conn.cursor()
-        cur.execute("SELECT item FROM info1 WHERE item LIKE %s ORDER BY item LIMIT 10", ('%' + search_query.replace(' ', '%') + '%',))
-        suggested_words = [row[0] for row in cur.fetchall()]
-        return suggested_words
-    return False
+        return render_template("search.html", suggested_words=suggested_words)
+    else:
+        return render_template("search.html")
 
-@app.route('/get_suggestions', methods=['POST'])
+
+@app.route("/get_suggestions", methods=["POST"])
 def get_suggestions():
-    search_query = request.json['search_query']
-    suggested_words = retrieve_suggested_words_from_database(search_query)
-    return jsonify({'suggested_words': suggested_words})
+    search_query = request.json["search_query"]
+    suggested_words = helper.get_suggestions(search_query)
+    return jsonify({"suggested_words": suggested_words})
+
 
 @app.route("/location")
 def location():
-    json_data = []
+    data = mongo_q.get_locations()
 
-    location_result = mongo_db.get_collection("location")
-    for data in location_result.find():
-        json_data.append(data)
+    return render_template("location.html", username=session["username"], data=data)
 
-    return render_template('location.html', username=session['username'],data=json_data)
-    
-@app.route('/scan', methods=['GET', 'POST'])
+
+@app.route("/scan", methods=["GET", "POST"])
 def scan():
-    queryBinID = request.args.get('qb')  # Access the 'qb' query parameter
-    queryLocation = request.args.get('ql')  # Access the 'ql' query parameter
+    queryBinID = request.args.get("qb")  # Access the 'qb' query parameter
+    queryLocation = request.args.get("ql")  # Access the 'ql' query parameter
+
     if queryBinID and queryLocation:
-        if request.method == 'POST' and 'materialType' in request.form:
-            materialType = request.form['materialType']
-            f = request.files['fileInput']
-            if f.filename != '':
+        if request.method == "POST" and "materialType" in request.form:
+            materialType = request.form["materialType"]
+            f = request.files["fileInput"]
+            if f.filename != "":
                 fdir = "uploads/" + f.filename
                 f.save(fdir)
-                db.recyle(queryBinID, fdir, materialType)
-                msg = 'You have successfully recycled!'
-                return redirect(url_for('home'))
+                maria_q.recyle(queryBinID, fdir, materialType)
+                msg = "You have successfully recycled!"
+                return redirect(url_for("home"))
             else:
-                db.recyle(queryBinID, '', materialType)
-                msg = 'You have successfully recycled!'
-                return redirect(url_for('home'))
-        elif request.method == 'POST':
-            msg = 'Please fill in all the fields!'
-            return redirect(url_for('register'))
-        return render_template('form.html', queryBinID=queryBinID, queryLocation=queryLocation)
+                maria_q.recyle(queryBinID, "", materialType)
+                msg = "You have successfully recycled!"
+                return redirect(url_for("home"))
+        elif request.method == "POST":
+            msg = "Please fill in all the fields!"
+            return redirect(url_for("register"))
+
+        return render_template(
+            "form.html", queryBinID=queryBinID, queryLocation=queryLocation
+        )
     else:
-        return render_template('scan.html')
+        return render_template("scan.html")
+
 
 @app.route("/logout")
 def logout():
-    session.pop('loggedin',None)
-    session.pop('id',None)
-    session.pop('username',None)
-    return redirect(url_for('login'))
+    session.pop("loggedin", None)
+    session.pop("id", None)
+    session.pop("username", None)
+    return redirect(url_for("login"))
 
-if __name__ == '__main__':
-    app.run(debug=True, ost='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True, ost="0.0.0.0", port=5000)
---
+++ src/utils/mariadb_queries.py
@@ -0,0 +1,98 @@
+from database.mariadb_conn import MariaDBConnManager
+
+from flask import session
+
+maria_db = MariaDBConnManager()
+
+
+# login page
+def authenticate(username, pwd):
+    query = "SELECT * FROM Users WHERE username=%s AND password=%s"
+    result = maria_db.execute(query, "one", username, pwd)
+    return result["result"]
+
+
+# register page
+def regCheck(username):
+    query = "SELECT * FROM Users WHERE username = %s"
+    result = maria_db.execute(query, "one", username)
+    return result["result"]
+
+
+def register(name, email, area, username, pwd):
+    query = "INSERT INTO Users (Name, Email, Area, Username, Password, isAdmin, Points) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    result = maria_db.execute(query, "", name, email, area, username, pwd, False, 0)
+    return result["result"]
+
+
+# home page
+def getUserPoints():
+    query = "SELECT Points FROM Users WHERE UserID = %s"
+    result = maria_db.execute(query, "one", session["id"])
+    return result["result"]
+
+
+def getRecyclesByMonth():
+    query = "SELECT DATE(Datetime) AS recyDate, COUNT(*) AS NumRecords FROM Recycles WHERE UserID = %s GROUP BY recyDate ORDER BY recyDate"
+    result = maria_db.execute(query, "all", session["id"])
+    return result["result"]
+
+
+def getMaterialCount():
+    query = "SELECT MaterialType, COUNT(*) AS matCount FROM Recycles WHERE UserID = %s GROUP BY MaterialType"
+    result = maria_db.execute(query, "all", session["id"])
+    return result["result"]
+
+
+# profile page
+def updateProfile(name, email, area, username):
+    query = "UPDATE Users SET name=%s, email=%s, area=%s, username=%s WHERE UserID = %s"
+    result = maria_db.execute(query, "", name, email, area, username, session["id"])
+    return result["result"]
+
+
+def updateProfileWPwd(name, email, area, username, password):
+    query = "UPDATE Users SET name=%s, email=%s, area=%s, username=%s, password=%s WHERE UserID = %s"
+    result = maria_db.execute(
+        query, "", name, email, area, username, password, session["id"]
+    )
+    return result["result"]
+
+
+def userProfile():
+    query = "SELECT * FROM Users WHERE UserID=%s"
+    result = maria_db.execute(query, "one", session["id"])
+    return result["result"]
+
+
+# scan page
+def recyle(queryBinID, fdir, materialType):
+    query = "INSERT INTO Recycles (RecycledID, BinID, Datetime, Image, MaterialType, UserID) VALUES (%s, %s, NOW(), %s, %s, %s)"
+    result = maria_db.execute(
+        query, "", "", queryBinID, fdir, materialType, session["id"]
+    )
+    return result["result"]
+
+
+# search page
+def get_search(query_param, value):
+    if query_param == "q":
+        query = "SELECT * FROM info1 INNER JOIN info2 ON info1.id = info2.id WHERE info1.item = %s"
+        fetch_method = "one"
+        result = maria_db.execute(query, fetch_method, value)["result"]
+    else:
+        query = "SELECT * FROM info1 INNER JOIN info2 ON info1.id = info2.id WHERE info1.type = %s"
+        fetch_method = "all"
+        result = maria_db.execute(query, fetch_method, value)
+
+        row_headers = [x[0] for x in result["description"]]
+        result = [dict(zip(row_headers, r)) for r in result["result"]]
+
+    return result
+
+
+def suggestion(search_query):
+    query = "SELECT item FROM info1 WHERE item LIKE %s ORDER BY item LIMIT 10"
+    arg = "%" + search_query.replace(" ", "%") + "%"
+    result = maria_db.execute(query, "all", arg)
+    return result["result"]
